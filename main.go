fb3915422d22d8379c706ab04de0e942a47d9a52
Enocp <pierre.enoc@gmail.com>
2021-10-06 21:28:47 -0300

    :recycle: refactor

--- main.go
+++ main.go
@@ -5,11 +5,22 @@ import (
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Bem vindo")
-	})
+func rotaPrincipal(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Bem vindo")
+}
 
+func configuraRotas() {
+	http.HandleFunc("/", rotaPrincipal)
+
+}
+
+func configurarSevidor() {
+	configuraRotas()
+
+	fmt.Println("Servidor esta rodando na porta 1337")
 	http.ListenAndServe(":1337", nil) // DefaulServerMux
+}
 
+func main() {
+	configurarSevidor()
 }
