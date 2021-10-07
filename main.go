ad989641c1af9f44544b88e8c144eadac0fb274f
Enocp <pierre.enoc@gmail.com>
2021-10-06 22:49:55 -0300

    add content-type

--- main.go
+++ main.go
@@ -3,31 +3,32 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 )
 
 type Livro struct {
-	Id      int
-	TItulo  string
-	Autotor string
+	Id     int    `json:"id"`
+	Titulo string `json:"titulo"`
+	Autor  string `json:"autor"`
 }
 
 var Livros []Livro = []Livro{
 	Livro{
-		Id:      1,
-		TItulo:  "O Guarani",
-		Autotor: "Pierre",
+		Id:     1,
+		Titulo: "O Guarani",
+		Autor:  "Pierre",
 	},
 
 	Livro{
-		Id:      2,
-		TItulo:  "Camikase",
-		Autotor: "Helena",
+		Id:     2,
+		Titulo: "Camikase",
+		Autor:  "Helena",
 	},
 	Livro{
-		Id:      3,
-		TItulo:  "Dom casmura",
-		Autotor: "Machado de Assis",
+		Id:     3,
+		Titulo: "Dom casmura",
+		Autor:  "Machado de Assis",
 	},
 }
 
@@ -36,6 +37,7 @@ func rotaPrincipal(w http.ResponseWriter, r *http.Request) {
 }
 
 func listarLivros(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 	encoder := json.NewEncoder(w)
 	encoder.Encode(Livros)
 }
@@ -50,7 +52,7 @@ func configurarSevidor() {
 	configuraRotas()
 
 	fmt.Println("Servidor esta rodando na porta 1337")
-	http.ListenAndServe(":1337", nil) // DefaulServerMux
+	log.Fatal(http.ListenAndServe(":1337", nil)) // DefaulServerMux
 }
 
 func main() {
