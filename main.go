9c1a615189aa71205c838d452db66235106cc3ec
Enocp <pierre.enoc@gmail.com>
2021-10-06 21:56:04 -0300

    :recycle: livros created

--- main.go
+++ main.go
@@ -1,16 +1,48 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
+
+type Livro struct {
+	Id      int
+	TItulo  string
+	Autotor string
+}
+
+var Livros []Livro = []Livro{
+	Livro{
+		Id:      1,
+		TItulo:  "O Guarani",
+		Autotor: "Pierre",
+	},
+
+	Livro{
+		Id:      2,
+		TItulo:  "Camikase",
+		Autotor: "Helena",
+	},
+	Livro{
+		Id:      3,
+		TItulo:  "Dom casmura",
+		Autotor: "Machado de Assis",
+	},
+}
 
 func rotaPrincipal(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Bem vindo")
 }
 
+func listarLivros(w http.ResponseWriter, r *http.Request) {
+	encoder := json.NewEncoder(w)
+	encoder.Encode(Livros)
+}
+
 func configuraRotas() {
 	http.HandleFunc("/", rotaPrincipal)
+	http.HandleFunc("/livros", listarLivros)
 
 }
 
